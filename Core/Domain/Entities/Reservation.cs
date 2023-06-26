225280f036b8cbc624e359b8be62edbd1b1fb00a
Nguyễn Tuấn Lộc <74365102+NgTuanLoc@users.noreply.github.com>
2023-06-26 21:19:45 +0700

    revert entity

--- Core/Domain/Entities/Reservation.cs
+++ Core/Domain/Entities/Reservation.cs
@@ -5,7 +5,7 @@ namespace Core.Domain.Entities
    public class Reservation : BaseModel
    {
       public ApplicationUser? User { get; set; }
-      public Room Room { get; set; } = default!;
+      public Room? Room { get; set; }
       public DateTime StartDate { get; set; }
       public DateTime EndDate { get; set; }
       public float Price { get; set; }
--- Core/Domain/Entities/Review.cs
+++ Core/Domain/Entities/Review.cs
@@ -4,8 +4,8 @@ namespace Core.Domain.Entities
 {
    public class Review : BaseModel
    {
-      public Reservation Reservation { get; set; } = default!;
-      public ApplicationUser User { get; set; } = default!;
+      public Reservation? Reservation { get; set; }
+      public ApplicationUser? User { get; set; }
       public int Rating { get; set; }
       public string Comment { get; set; } = "No Comment";
       public string Title { get; set; } = "No Title";
